c5316840d3bac0aa560d98e72ec5163ed1305b14
Mabry Cervin <mabry.cervin@gmail.com>
2018-01-24 20:57:05 -0500

    Did more stuff

--- Main.elm
+++ Main.elm
@@ -2,6 +2,8 @@ module Main exposing (main)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Task
 
 main : Program Never Model Msg
 main =
@@ -13,7 +15,8 @@ main =
         }
 
 type alias Model =
-    { projects : List Project
+    { projects : List Project,
+      nameContent : String
     }
 
 type alias Project =
@@ -25,22 +28,43 @@ type alias Project =
 emptyModel : Model
 emptyModel =
     { projects = []
+    , nameContent = ""
     }
 
 init : (Model, Cmd Msg)
 init = emptyModel ! []
 
 type Msg
-    = NoOp
+    = UpdateName String
+    | Submit
+    | Send
+    | Success
+    | Noop
+
+sendProject : String -> Task Error String
+sendProject name = post Decode.string "http://localhost" empty
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        NoOp ->
-            model ! []
+        UpdateName name ->
+            {model | nameContent = name} ! []
+        Submit ->
+            let
+                project = {name = model.nameContent, url = "", readmeSnippet = ""}
+            in
+                {model | projects = project::model.projects} ! []
+        Send ->
+            let
+                handleError _ = Noop
+                handleVal _ = Success
+            in
+                Task.perform handleError handleVal <| sendProject
 
 view : Model -> Html Msg
 view model =
-    div
-        [ class "dashboard-wrapper" ]
-        [ text "Success!" ]
+    div []
+        [ input [ type_ "text", placeholder "Test", onInput UpdateName ] []
+        , button [ onClick Submit ] [ text "Submit" ]
+        , div [] [ text <| Maybe.withDefault "" <| Maybe.map .name <| List.head model.projects ]
+        ]
